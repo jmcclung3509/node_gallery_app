a52e4c0c366c4df9b5f9b1e3a2d4a300529756b1
Jessica Turner <jmcclung3509@gmail.com>
2022-09-01 22:43:00 -0700

    updated pages-routes

--- application/pages-routes.js
+++ application/pages-routes.js
@@ -19,6 +19,58 @@ router.get('/', async (req, res) => {
     }
 
 })
+router.get('/search', async (req, res) => {
+
+    const data = await Super.find({
+        $or: [
+            { name: { '$regex': req.query.dsearch } },
+            { description: { '$regex': req.query.dsearch } },
+            { powers: { '$regex': req.query.dsearch } },
+            { weakness: { '$regex': req.query.dsearch } },
+            { category: { '$regex': req.query.dsearch } },
+            { appearance: { '$regex': req.query.dsearch } },
+            { description: { '$regex': req.query.dsearch } },
+            { aka: { '$regex': req.query.dsearch } }
+        ]
+    })
+    console.log(data)
+
+    res.render('search', { data });
+})
+router.post('/search', async (req, res) => {
+
+    res.redirect('/search?dsearch=' + req.body.dsearch);
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const id = req.params['id'];
+    let response = await Super.deleteOne({ id })
+    // req.flash("success", 'Record deleted');
+    res.send('/super');
+
+})
+router.get('/edit/:id', async (req, res) => {
+    const id = req.params['id'];
+    const data = await Super.findOne({ id });
+
+    res.send('edit-page', {
+        data,
+    });
+
+
+})
+router.post('/edit-page/:id', async (req, res) => {
+    const id = req.params['id'];
+    const payload = req.body;
+    const response = await Super.updateOne({ id }, payload);
+    // res.send(payload)
+    res.send('/super');
+
+
+});
+router.get('/add-page', (req, res) => {
+    res.send({data:response})
+})
 
 router.post('/', async (req, res) => {
     try {
